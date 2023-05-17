7e028a4205e6a0e1954e174ee40c155cec4a17b0
abasch3 <abasch3@gatech.edu>
2023-05-17 00:13:07 -0400

    the commit right before this looked great

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
-// var slideIndex = 1;
-// showDivs(slideIndex);
-
-// function plusDivs(n) {
-//   showDivs(slideIndex += n);
-// }
-
-// function currentDiv(n) {
-//   showDivs(slideIndex = n);
-// }
-
-// function showDivs(n) {
-//   var i;
-//   var x = document.getElementsByClassName("project");
-//   var dots = document.getElementsByClassName("dot");
-//   if (n > x.length) {slideIndex = 1}
-//   if (n < 1) {slideIndex = x.length}
-//   for (i = 0; i < x.length; i++) {
-//     x[i].style.display = "none";  
-//   }
-//   for (i = 0; i < dots.length; i++) {
-//     dots[i].className = dots[i].className.replace("white", "");
-//   }
-//   x[slideIndex-1].style.display = "block";  
-//   dots[slideIndex-1].className += "white";
-// }
+var slideIndex = 1;
+	showSlide(slideIndex);
+	
+	function changeSlide(n) {
+	   showSlide(slideIndex += n);
+	}
+	
+	function showSlide(n) {
+		var slides = document.getElementsByClassName("project");
+		var i;
+		
+		for(i = 0; i < slides.length; i++) {
+			slides[i].className = "project";
+		}
+		
+		if(n > slides.length) {
+			slideIndex = 1;
+		}
+		
+		else if(n < 1) {
+			slideIndex = slides.length;
+		}
+		
+		slides[slideIndex - 1].className = "project active";
+		
+	}
