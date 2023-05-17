a12d903b2d44e212a3e93f82280c115d7990b9e1
abasch3 <abasch3@gatech.edu>
2023-05-16 23:45:49 -0400

    updates

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
-var slideIndex = 1;
-showDivs(slideIndex);
+// var slideIndex = 1;
+// showDivs(slideIndex);
 
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
+// function plusDivs(n) {
+//   showDivs(slideIndex += n);
+// }
 
-function currentDiv(n) {
-  showDivs(slideIndex = n);
-}
+// function currentDiv(n) {
+//   showDivs(slideIndex = n);
+// }
 
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("project");
-  var dots = document.getElementsByClassName("dot");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace("white", "");
-  }
-  x[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += "white";
-}
+// function showDivs(n) {
+//   var i;
+//   var x = document.getElementsByClassName("project");
+//   var dots = document.getElementsByClassName("dot");
+//   if (n > x.length) {slideIndex = 1}
+//   if (n < 1) {slideIndex = x.length}
+//   for (i = 0; i < x.length; i++) {
+//     x[i].style.display = "none";  
+//   }
+//   for (i = 0; i < dots.length; i++) {
+//     dots[i].className = dots[i].className.replace("white", "");
+//   }
+//   x[slideIndex-1].style.display = "block";  
+//   dots[slideIndex-1].className += "white";
+// }
